9cdd0ec1123191217f824a805d73f2d8fddd8b53
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-02-28 23:54:39 +0530

    Update Navbar component

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,6 +1,19 @@
-
+import Image from 'next/image'
+import styles from '../styles/Navbar.module.css'
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width='32' height='32'/>
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>012 345 678</div>
+        </div>
+      </div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
   )
 }
