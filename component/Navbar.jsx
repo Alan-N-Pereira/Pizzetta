e7117466a25097e4e8d656315ef36c37422105f5
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-03-03 23:59:06 +0530

    Update Navbar component

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -12,8 +12,23 @@ export default function Navbar() {
           <div className={styles.text}>012 345 678</div>
         </div>
       </div>
-      <div className={styles.item}>center</div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src='/img/logo.png' alt='' width='160px' height='96px' />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src='/img/cart.png' alt='' width='30px' height='30px' />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   )
 }
