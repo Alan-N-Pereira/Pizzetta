0136fdbf9c46ca6e30a83266793958c6630579f0
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-03-18 23:31:44 +0530

    Add Footer Component

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -1,7 +1,50 @@
 import Image from 'next/image'
-// import styles from '../styles/Footer.module.css'
+import styles from '../styles/Footer.module.css'
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src='/img/bg.png' layout='fill' objectFit='cover' alt=''/>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>OH YES, WE DID IT PIZZETTA PIZZA, WELL BAKED SLICE OF PIZZA.</h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            165 R. Don Road #304.
+            <br />NewYork, 85022.
+            <br />(602) 867-1010
+          </p>
+          <p className={styles.text}>
+            165 R. Don Road #235.
+            <br />NewYork, 85022.
+            <br />(602) 867-1011
+          </p>
+          <p className={styles.text}>
+            165 R. Don Road #104.
+            <br />NewYork, 85022.
+            <br />(602) 867-1012
+          </p>
+          <p className={styles.text}>
+            165 R. Don Road #125.
+            <br />NewYork, 85022.
+            <br />(602) 867-1013
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 - 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00 - 24:00
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
