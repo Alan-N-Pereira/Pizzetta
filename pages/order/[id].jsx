c59c13d94aa079a9120f854ab307fdc68d6ac94e
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-03-28 11:03:56 +0530

    Update order component and style

--- pages/order/[id].jsx
+++ pages/order/[id].jsx
@@ -2,6 +2,12 @@ import Image from 'next/image'
 import styles from '../../styles/Order.module.css'
 
 export default function Order() {
+    const status = 0
+    const statusClass = (index) =>{
+        if(index-status < 1) return styles.done
+        if(index-status === 1) return styles.inProgress
+        if(index-status > 1) return styles.notDone
+    }
   return (
     <div className={styles.container}>
 
@@ -30,7 +36,28 @@ export default function Order() {
                     </tr>
                 </table>
             </div>
-            <div className={styles.row}></div>
+            <div className={styles.row}>
+                <div className={statusClass(0)}>
+                    <Image src='/img/paid.png' alt='' width={30} height={30} />
+                    <span>Payment</span>
+                    <div className={styles.checkedIcon}><Image src='/img/checked.png' alt='' width={20} height={20} /></div>
+                </div>
+                <div className={statusClass(1)}>
+                    <Image src='/img/bake.png' alt='' width={30} height={30} />
+                    <span>Preparing</span>
+                    <div className={styles.checkedIcon}><Image src='/img/checked.png' alt='' width={20} height={20} /></div>
+                </div>
+                <div className={statusClass(2)}>
+                    <Image src='/img/bike.png' alt='' width={30} height={30} />
+                    <span>On the way</span>
+                    <div className={styles.checkedIcon}><Image src='/img/checked.png' alt='' width={20} height={20} /></div>
+                </div>
+                <div className={statusClass(3)}>
+                    <Image src='/img/delivered.png' alt='' width={30} height={30} />
+                    <span>Delivered</span>
+                    <div className={styles.checkedIcon}><Image src='/img/checked.png' alt='' width={20} height={20} /></div>
+                </div>
+            </div>
         </div>
 
         <div className={styles.right}>
