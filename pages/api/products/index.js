409d7ade8a4c539f7ea33297fdc0247500ab381c
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-05-03 23:58:41 +0530

    Update product api get method

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -7,6 +7,12 @@ export default async function handler(req, res) {
 	dbConnect();
 
 	if (method === 'GET') {
+		try {
+			const products = await Product.find();
+			res.status(200).json(products);
+		} catch (err) {
+			res.status(500).json(err);
+		}
 	}
 	if (method === 'POST') {
 		try {
