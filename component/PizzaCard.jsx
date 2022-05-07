c3e34ac65479c2761008fe02cbfe653c11791800
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-05-07 23:53:22 +0530

    Add Data to pizza card list

--- component/PizzaCard.jsx
+++ component/PizzaCard.jsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import styles from '../styles/PizzaCard.module.css'
+import Link from 'next/link'
 
-export default function PizzaCard() {
+export default function PizzaCard({pizza}) {
   return (
-    <div className={styles.container}>
-      <Image src='/img/pizza.png' alt='' width='500' height='500' />
-      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
-      <span className={styles.price}>$19.90</span>
-      <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
-    </div>
+    <Link href={`/product/${pizza._id}`} passHref>
+      <div className={styles.container}>
+        <Image src={pizza.img} alt='' width='500' height='500' />
+        <h1 className={styles.title}>{pizza.title}</h1>
+        <span className={styles.price}>{pizza.prices[0]}</span>
+        <p className={styles.desc}>{pizza.desc}</p>
+      </div>
+    </Link>
   )
 }
--- component/ProducttList.jsx
+++ component/ProducttList.jsx
@@ -2,18 +2,15 @@ import Image from 'next/image'
 import styles from '../styles/ProductList.module.css'
 import PizzaCard from './PizzaCard'
 
-export default function ProducttList() {
+export default function ProducttList({pizzaList}) {
   return (
     <div className={styles.container}>
         <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
         <p className={styles.desc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro quos illo, ducimus, quis sapiente dolores cumque quae enim inventore aliquam natus? Consectetur voluptatibus odio fuga eius inventore illo soluta iure.</p>
         <div className={styles.wrapper}>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
-            <PizzaCard/>
+            {pizzaList.map((pizza)=>{
+              <PizzaCard key={pizza.id} pizza={pizza}/>
+            })}
         </div>
     </div>
   )
