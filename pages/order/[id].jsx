dfa0b8f7ed85327a80adc42088b95bf10ce01fd3
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-07-23 22:28:32 +0530

    Update cart order

--- pages/order/[id].jsx
+++ pages/order/[id].jsx
@@ -65,13 +65,13 @@ export default function Order({order}) {
             <div className={styles.wrapper}>
                 <h2 className={styles.title}>CART TOTAL</h2>
                 <div className={styles.totalText}>
-                    <b className={styles.totalTextTitle}>Subtotal</b>$79.60
+                    <b className={styles.totalTextTitle}>Subtotal</b>${order.total}
                 </div>
                 <div className={styles.totalText}>
                     <b className={styles.totalTextTitle}>Discount</b>$0.00
                 </div>
                 <div className={styles.totalText}>
-                    <b className={styles.totalTextTitle}>Total</b>$79.60
+                    <b className={styles.totalTextTitle}>Total</b>${order.total}
                 </div>
                 <button disabled className={styles.button}>PAID</button>
             </div>
