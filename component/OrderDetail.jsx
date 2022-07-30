959eb192bc0cc0f56e5c9edb71c62a5be1af5c77
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-07-30 15:49:23 +0530

    Update OrderDetail component

--- component/OrderDetail.jsx
+++ component/OrderDetail.jsx
@@ -3,6 +3,10 @@ import styles from '../styles/OrderDetail.module.css'
 export default function OrderDetail() {
     const [customer,setCustomer]= useState('')
     const [address,setAddress]= useState('')
+
+    const handleClick = ()=>{
+      
+    }
   return (
     <div className={styles.container}>
         <div className={styles.wrapper}>
@@ -11,6 +15,15 @@ export default function OrderDetail() {
                 <label className={styles.label}>Name Surname</label>
                 <input type="text" placeholder='John Doe' className={styles.input} onChange={(e)=>setCustomer(e.target.value)}/>
             </div>
+            <div className={styles.item}>
+                <label className={styles.label}>Phone Number</label>
+                <input type="text" placeholder='+1 234 567 89' className={styles.input}/>
+            </div>
+            <div className={styles.item}>
+                <label className={styles.label}>Address</label>
+                <textarea type="text" placeholder='Elton St. 505 NY' rows={5} className={styles.textarea} onChange={(e)=>setAddress(e.target.value)}/>
+            </div>
+            <button className={styles.button} onClick={handleClick}>Order</button>
         </div>
     </div>
   )
