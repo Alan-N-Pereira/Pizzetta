055cd8d718a84498197c8b9b74eb880cd8f46151
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-08-06 10:23:47 +0530

    Update admin component

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,6 +1,35 @@
+import Image from 'next/image'
 import styles from '../../styles/Admin.module.css'
 export default function Admin() {
   return (
-    <div className={styles.container}>Admin</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Products</h1>
+        <table className={styles.table}>
+          <tbody>
+            <tr className={styles.trTitle}>
+              <th>Image</th>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr className={styles.trTitle}>
+              <td><Image src='/img/pizza.png' width={50} height={50} objectFit='cover' /></td>
+              <td>PizzaId</td>
+              <td>Pizza Title</td>
+              <td>$50</td>
+              <td>
+                <button className={styles.button}>Edit</button>
+                <button className={styles.button}>Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.item}></div>
+    </div>
   )
 }
