52eb3142e136f4aaa0bafdc628a6141faf0a47a5
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-08-12 10:30:16 +0530

    Update admin order

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -5,11 +5,23 @@ import styles from '../../styles/Admin.module.css'
 export default function Admin({products,orders}) {
   const [pizzaList,setPizzaList] = useState(products)
   const [orderList,setOrderList] = useState(orders)
+  const status = ['Preparing', 'On the way', 'Delivered']
 
   const handleDelete = async (id)=>{
     try {
       const res = await axios.delete(`http://localhost:3000/api/products/${id}`)
       setPizzaList(pizzaList.filter((pizza)=>pizza._id !== id))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleStatus = async (id)=>{
+    const item = orderList.filter(order=> order._id === id)[0]
+    const currentStatus = item.status
+    try {
+      const res = await axios.put(`http://localhost:3000/api/orders/${id}`,{status: currentStatus+1})
+      setOrderList(res.data,...orderList.filter(order=>order._id !== id))
     } catch (error) {
       console.log(error)
     }
@@ -29,7 +41,7 @@ export default function Admin({products,orders}) {
               <th>Action</th>
             </tr>
           </tbody>
-            {pizzaList.map((product)=>{
+            {pizzaList.map((product)=>(
           <tbody key={product._id}>
               <tr className={styles.trTitle}>
               <td><Image src={product.img} width={50} height={50} objectFit='cover' /></td>
@@ -42,7 +54,7 @@ export default function Admin({products,orders}) {
               </td>
             </tr>
           </tbody>
-            })}
+            ))}
         </table>
       </div>
 
@@ -59,18 +71,20 @@ export default function Admin({products,orders}) {
                 <th>Action</th>
               </tr>
             </tbody>
-            <tbody>
-              <tr className={styles.trTitle}>
-                <td>57296870392918</td>
-                <td>John Doe</td>
-                <td>$50</td>
-                <td>paid</td>
-                <td>preparing</td>
-                <td>
-                  <button className={styles.button}>Next Stage</button>
-                </td>
-              </tr>
-            </tbody>
+            {orderList.map(order=>(
+              <tbody key={order._id}>
+                <tr className={styles.trTitle}>
+                  <td>{order._id}</td>
+                  <td>{order.customer}</td>
+                  <td>${order.total}</td>
+                  <td>{order.method === 0 ? <span>cash</span> : <span>paid</span>}</td>
+                  <td>{status[order.status]}</td>
+                  <td>
+                    <button className={styles.button} onClick={()=>handleStatus(order._id)}>Next Stage</button>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
         </table>
 
       </div>
