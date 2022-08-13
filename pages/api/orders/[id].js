fce0848652cd0716b05ca42afa806c2aa0633251
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-08-13 10:52:40 +0530

    Add order api put method

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -15,6 +15,12 @@ const handler = async (req, res) => {
 		}
 	}
 	if (method === 'PUT') {
+		try {
+			const order = await Order.findByIdAndUpdate(id, req.body, { new: true });
+			res.status(201).json(order);
+		} catch (err) {
+			res.status(500).json(err);
+		}
 	}
 	if (method === 'DELETE') {
 	}
--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
 	}
 	if (method === 'PUT') {
 		try {
-			const product = await Product.create(req.body);
+			const product = await Product.findByIdAndUpdate(id, req.body, { new: true });
 			res.status(201).json(product);
 		} catch (err) {
 			res.status(500).json(err);
