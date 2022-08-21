3a59049b4aeae7be591fa962db2364ff8de093a2
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-08-21 19:20:54 +0530

    Update admin login component

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -1,6 +1,20 @@
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 import styles from '../../styles/Login.module.css'
 export default function Login() {
+  const [username, setUsername] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [error, setError] = useState(false)
+  const router = useRouter()
+
   return (
-    <div>Login</div>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.head}>Admin Dashboard</h1>
+        <input className={styles.input} placeholder='username' onChange={(e)=> setUsername(e.target.value)} />
+        <input className={styles.input} placeholder='password' type='password' onChange={(e)=> setPassword(e.target.value)} />
+        <button className={styles.button} onClick={handleClick}>Sign In</button>
+      </div>
+    </div>
   )
 }
