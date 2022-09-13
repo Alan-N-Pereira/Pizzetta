10f2f642989e9e18a6af662eed7b619d04752abe
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-09-13 21:29:26 +0530

    Update addbutton component

--- component/AddButton.jsx
+++ component/AddButton.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Add.module.css'
-export default function AddButton() {
+export default function AddButton({setClose}) {
   return (
-    <div className={styles.mainAddButton}>Add New Pizza</div>
+    <div className={styles.mainAddButton} onClick={()=> setClose(false)}>Add New Pizza</div>
   )
 }
