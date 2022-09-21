8ea0cdb9297b003a973f0fde6e7e5e497840600c
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-09-21 22:27:47 +0530

    Update Add component

--- component/Add.jsx
+++ component/Add.jsx
@@ -1,6 +1,18 @@
-
-export default function Add() {
+import { useState } from "react"
+import styles from '../styles/Add.module.css'
+import axios from "axios"
+import { useRouter } from "next/router"
+export default function Add({setClose}) {
   return (
-    <div>Hello</div>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <span className={styles.close} onClick={()=>setClose(true)}>X</span>
+        <h1>Add a new Pizza</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Choose an image</label>
+          <input type="file" />
+        </div>
+      </div>
+    </div>
   )
 }
