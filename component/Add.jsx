e98f3fb856cfdb9b8517d89b50a30096f18ac88a
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-10-10 19:33:05 +0530

    Update extra input in Add component

--- component/Add.jsx
+++ component/Add.jsx
@@ -13,25 +13,39 @@ export default function Add({setClose}) {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
+
         <span className={styles.close} onClick={()=>setClose(true)}>X</span>
         <h1>Add a new Pizza</h1>
+
         <div className={styles.item}>
           <label className={styles.label}>Choose an image</label>
           <input type="file" />
         </div>
+
         <div className={styles.item}>
           <label className={styles.label}>Title</label>
           <input className={styles.input} type="text" onChange={(e)=>setTitle(e.target.value)} />
         </div>
+
         <div className={styles.item}>
           <label className={styles.label}>Desc</label>
           <textarea rows={4} type="text" onChange={(e)=>setDesc(e.target.value)} />
         </div>
+
         <div className={styles.item}>
           <label className={styles.label}>Prices</label>
           <input className={`${styles.input} ${styles.inputS}`} type='number' placeholder="Small" onChange={(e)=>changePrice(e,0)} />
           <input className={`${styles.input} ${styles.inputS}`} type='number' placeholder="Medium" onChange={(e)=>changePrice(e,1)} />
           <input className={`${styles.input} ${styles.inputS}`} type='number' placeholder="Large" onChange={(e)=>changePrice(e,2)} />
+        </div>
+
+        <div className={styles.item}>
+          <label className={styles.label}>Extra</label>
+          <div className={styles.extra}>
+            <input className={`${styles.input} ${styles.inputS}`} type="text" placeholder="Item" name="text" onChange={handleExtraInput} />
+            <input className={`${styles.input} ${styles.inputS}`} type="number" placeholder="Price" name="price" onChange={handleExtraInput} />
+            <button className={styles.extraButton} onClick={handleExtra}>Add</button>
+          </div>
         </div>
       </div>
     </div>
