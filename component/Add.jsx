348edb4c88194f518b25ab7cc058d0ce3d206887
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-10-10 19:16:59 +0530

    Update add component

--- component/Add.jsx
+++ component/Add.jsx
@@ -3,6 +3,13 @@ import styles from '../styles/Add.module.css'
 import axios from "axios"
 import { useRouter } from "next/router"
 export default function Add({setClose}) {
+  const [file,setFile] = useState(null)
+  const [title,setTitle] = useState(null)
+  const [desc,setDesc] = useState(null)
+  const [prices,setPrices] = useState([])
+  const [extra,setExtra] = useState(null)
+  const [extraOptions,setExtraOptions] = useState([])
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -11,6 +18,14 @@ export default function Add({setClose}) {
         <div className={styles.item}>
           <label className={styles.label}>Choose an image</label>
           <input type="file" />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Title</label>
+          <input className={styles.input} type="text" onChange={(e)=>setTitle(e.target.value)} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Desc</label>
+          <textarea rows={4} type="text" onChange={(e)=>setDesc(e.target.value)} />
         </div>
       </div>
     </div>
