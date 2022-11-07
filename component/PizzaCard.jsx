af28fab257e36896c44f7a2a42b26c7f01f1a9b8
Alan-N-Pereira <itsalanpereira@gmail.com>
2022-11-07 10:07:46 +0530

    Update PizzaCard Component

--- component/PizzaCard.jsx
+++ component/PizzaCard.jsx
@@ -8,7 +8,7 @@ export default function PizzaCard({ pizza }) {
       <div className={styles.container}>
         <Image src={pizza.img} alt='' width='500' height='500' />
         <h1 className={styles.title}>{pizza.title}</h1>
-        <span className={styles.price}>{pizza.prices[0]}</span>
+        <span className={styles.price}>${pizza.prices[0]}</span>
         <p className={styles.desc}>{pizza.desc}</p>
       </div>
 
